f8370d153347f3c6593e7f0eb5d64aca96661ad9
Filip Lipowiecki <lipowiecki.f@gmail.com>
2018-07-30 11:52:42 +0200

    ButtonStarted

---
+++ CtlgEver/src/Components/Button/Button.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Button.scss';
+
+
+class Button extends Component {
+  render() {
+    return (
+      <div className="Button">
+      
+      </div>
+    );
+  }
+}
+
+export default Button;
